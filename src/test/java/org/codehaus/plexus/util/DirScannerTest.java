c8c20582a6d3786776cf600cb1d96f75c9267ad7
Kristian Rosenvold <kristian.rosenvold@gmail.com>
2011-12-16 17:55:49 +0100

    Fixed

---
+++ src/test/java/org/codehaus/plexus/util/DirScannerTest.java
@@ -0,0 +1,26 @@
+package org.codehaus.plexus.util;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Kristian Rosenvold
+ */
+public class DirScannerTest
+    extends TestCase
+{
+    public void testScan()
+        throws Exception
+    {
+/*
+        File file = new File("/home/kristian/fud1/");
+        assertFalse( file.isFile() );
+        assertTrue( file.isDirectory() );
+        file = new File("/home/kristian/fud2/");
+        assertFalse( file.isFile() );
+        assertTrue( file.isDirectory() );
+        file = new File("/home/kristian/fud3/");
+        assertTrue( file.isFile() );
+        assertFalse( file.isDirectory() );
+  */
+    }
+}
